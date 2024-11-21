ac0865da9474c15846c27b07b43e1c86110f8fb6
Fabricio <fa.engeroff1996@gmail.com>
2024-11-21 09:28:34 -0300

    Manipulei o fluxo dos input do formulario controlando os estados e cadastrando novos colaboradores.

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -2,9 +2,9 @@ import Botao from "../Botao";
 import CampoTexto from "../CampoTexto";
 import ListaSuspensa from "../ListaSuspensa";
 import "./Formulario.css";
+import { useState } from "react";
 
-const Formulario = () => {
-
+const Formulario = (props) => {
   const cargos = [
     "Diretor Geral",
     "Gerente de Qualidade",
@@ -17,20 +17,72 @@ const Formulario = () => {
     "Operador de Produção",
     "Representante Comercial",
   ];
+
+  const [nome, setNome] = useState("");
+  const [cargo, setCargo] = useState("");
+  const [telefone, setTelefone] = useState("");
+  const [email, setEmail] = useState("");
+  const [endereco, setEndereco] = useState("");
+
+  const aoSalvar = (evento) => {
+    evento.preventDefault();
+    props.colaboradorCadastrado({nome, cargo, telefone, email, endereco });
+  };
+
   return (
     <section className="formulario">
-      <form>
+      <form onSubmit={aoSalvar}>
         <h2>Preencha os dados para a plataforma</h2>
-        <CampoTexto label="Name" placeholder="Digite seu nome" />
-        <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
-        <CampoTexto label="Telefone" placeholder="Digite seu telefone" />
-        <CampoTexto label="Endereço" placeholder="Digite seu endereço" />
-        <ListaSuspensa label="Selecione o cargo"itens={cargos}/>
-        <Botao>
-          Criar Card
-          </Botao> 
+
+        <CampoTexto
+          obrigatorio={true}
+          label="Nome"
+          placeholder="Digite seu nome"
+          valor={nome}
+          aoAlterado={(valor) => setNome(valor)} 
+        />
+
+        <CampoTexto
+          obrigatorio={true}
+          label="Função"
+          placeholder="Digite sua função na empresa"
+          valor={cargo}
+          aoAlterado={(valor) => setCargo(valor)} 
+        />
+
+        <CampoTexto
+          label="Telefone"
+          placeholder="Digite seu telefone"
+          valor={telefone}
+          aoAlterado={(valor) => setTelefone(valor)} 
+        />
+
+        <CampoTexto
+          label="Email"
+          placeholder="Digite seu email"
+          valor={email}
+          aoAlterado={(valor) => setEmail(valor)}
+        />
+
+        <CampoTexto
+          label="Endereço"
+          placeholder="Digite seu endereço, cidade e estado"
+          valor={endereco}
+          aoAlterado={(valor) => setEndereco(valor)} 
+        />
+
+        <ListaSuspensa
+          obrigatorio={true}
+          label="Selecione seu Cargo"
+          itens={cargos}
+          valor={cargo}
+          aoAlterado={(valor) => setCargo(valor)} 
+        />
+
+        <Botao>Criar Card</Botao>
       </form>
     </section>
   );
 };
+
 export default Formulario;
